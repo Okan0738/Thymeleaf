2b74dbbb146dc227f421df654948bec6d7ae3058
Okan <co.okancelik@gmail.com>
2024-05-02 16:15:44 +0200

    Conditional Logic added.

--- part-1/src/main/java/com/okan/controller/StudentController.java
+++ part-1/src/main/java/com/okan/controller/StudentController.java
@@ -4,14 +4,14 @@ import com.okan.bootstrap.DataGenarator;
 import com.okan.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register") //
+  //  @RequestMapping(value = "/register",method = RequestMethod.GET) //
+    @GetMapping("/register")
     public String register(Model model){
         model.addAttribute("student", DataGenarator.createStudents());
 
@@ -22,7 +22,8 @@ public class StudentController {
     public String drop(){
         return "student/register";
     }
-    @RequestMapping("/welcome") //
+    @RequestMapping(value = "/welcome", method = RequestMethod.GET) //
+    @PostMapping("/welcome")
     public String welcome(@RequestParam String name){
 
         System.out.println(name);
