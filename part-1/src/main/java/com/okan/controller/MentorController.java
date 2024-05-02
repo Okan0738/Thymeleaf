8224d4b424cc29aca3d725613f54bc3ee29a565d
Okan <co.okancelik@gmail.com>
2024-05-02 21:44:18 +0200

    Conditional Logic added.

--- part-1/src/main/java/com/okan/controller/MentorController.java
+++ part-1/src/main/java/com/okan/controller/MentorController.java
@@ -1,19 +1,45 @@
 package com.okan.controller;
 
+import com.okan.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("mentor")
 public class MentorController {
-    @RequestMapping("/register") //
-    public String register(){
-        return "student/register";
+
+    @GetMapping("/register") //
+    public String register(Model model){
+
+        List<String> batchList= Arrays.asList("JD1","JD2","JD3");
+        model.addAttribute("batchList",batchList);
+
+
+        model.addAttribute("mentor", new Mentor());
+
+        return "mentor/mentor-register";
     }
 
     @RequestMapping("/drop") //
     public String drop(){
-        return "student/register";
+        return "mentor/mentor-register";
     }
+    @PostMapping("/confirm")
+    public String submitForm(){
+        return "mentor/mentor-confirmation";
+    }
+
+    @GetMapping("/confirm")
+    public String submitForm2(){
+        return "mentor/mentor-confirmation";
+    }
+
 
 }
